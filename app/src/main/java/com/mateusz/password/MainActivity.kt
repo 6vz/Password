42e41e9e37981d35c8c191dbf2beb77e65246455
Matt <mateuszbaranowski@protonmail.com>
2023-02-04 16:53:11 +0100

    Dodano końcowe sprawdzanie, oraz handlowanie pustych pól.

--- app/src/main/java/com/mateusz/password/MainActivity.kt
+++ app/src/main/java/com/mateusz/password/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.mateusz.password
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
@@ -7,6 +8,7 @@ import android.widget.Button
 import android.widget.CheckBox
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,15 +23,35 @@ class MainActivity : AppCompatActivity() {
         val numberIncluded = findViewById<CheckBox>(R.id.numberIncluded)
         val specialIncluded = findViewById<CheckBox>(R.id.specialIncluded)
 
-        button.setOnClickListener() {
+        button.setOnClickListener {
             val a = inputA.text.toString()
             val b = inputB.text.toString()
+
+            if (a.isEmpty() || b.isEmpty()) {
+                inputA.error = "Pole nie może być puste"
+                inputB.error = "Pole nie może być puste"
+                return@setOnClickListener
+            }
 
             passwordMatching.isChecked = a == b
             capitalLetter.isChecked = a.any { it.isUpperCase() }
             smallLetter.isChecked = a.any { it.isLowerCase() }
             numberIncluded.isChecked = a.any { it.isDigit() }
             specialIncluded.isChecked = a.any { !it.isLetterOrDigit() }
+
+            if (passwordMatching.isChecked && capitalLetter.isChecked && smallLetter.isChecked && numberIncluded.isChecked && specialIncluded.isChecked) {
+                val builder = android.app.AlertDialog.Builder(this)
+                builder.setTitle("Spełniono wszystkie wymagania")
+                builder.setMessage("Hasło spełnia wszystkie wymagania. Możesz bezpiecznie używać tego hasła")
+                builder.setPositiveButton("OK", null)
+                builder.show()
+            } else {
+                val builder = android.app.AlertDialog.Builder(this)
+                builder.setTitle("Nie spełniono wszystkich wymagań")
+                builder.setMessage("Hasło nie spełnia wszystkich wymagań. Zalecamy zmianę hasła według zaleceń. ")
+                builder.setPositiveButton("OK", null)
+                builder.show()
+            }
         }
 
     }

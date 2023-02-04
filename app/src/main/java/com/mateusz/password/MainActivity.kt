05299db8416cb5f6cbc50e3f9f4996103d1b42a4
Matt <mateuszbaranowski@protonmail.com>
2023-02-04 16:44:32 +0100

    Modyfikacja ID i definiowanie w Kodzie co jest czym... Czemu...

--- app/src/main/java/com/mateusz/password/MainActivity.kt
+++ app/src/main/java/com/mateusz/password/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.Button
+import android.widget.CheckBox
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,7 +15,11 @@ class MainActivity : AppCompatActivity() {
         val inputB = findViewById<EditText>(R.id.inputB)
         val button = findViewById<Button>(R.id.button)
 
-
+        val passwordMatching = findViewById<CheckBox>(R.id.passwordMatching)
+        val capitalLetter = findViewById<CheckBox>(R.id.capitalLetter)
+        val smallLetter = findViewById<CheckBox>(R.id.smallLetter)
+        val numberIncluded = findViewById<CheckBox>(R.id.numberIncluded)
+        val specialIncluded = findViewById<CheckBox>(R.id.specialIncluded)
 
         button.setOnClickListener() {
 
